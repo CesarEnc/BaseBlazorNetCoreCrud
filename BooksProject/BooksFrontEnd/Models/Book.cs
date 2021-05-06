3b652d35ab350726c3ff9b395d892927005e39c6
CesarEnc <cesaraencarnacionm@gmail.com>
2021-05-06 01:17:20 -0400

    Created Blazor App, Books Service and Books View

---
+++ BooksProject/BooksFrontEnd/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksFrontEnd.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int pageCount { get; set; }
+        public string excerpt { get; set; }
+        public DateTime publishDate { get; set; }
+    }
+}
---
+++ BooksProject/BooksFrontEnd/Services/BooksService.cs
@@ -0,0 +1,45 @@
+﻿using BooksFrontEnd.Models;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BooksFrontEnd.Services
+{
+    public class BooksService : IBooksService
+    {
+        private HttpClient _client;
+        private readonly string _baseUri = Environment.GetEnvironmentVariable("BaseApiPoint");
+        public BooksService()
+        {
+            _client = new HttpClient();
+        }
+        public Task Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(int id) => await _client.DeleteAsync($"{_baseUri}Books/{id}");
+        
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            string test = $"{_baseUri}Books/";
+            var result = await _client.GetStringAsync(test);
+            return JsonConvert.DeserializeObject<List<Book>>(result);
+        }
+
+        public Task<Book> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(int id, Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
